38f0064241e2217d816b9c04c831724c72127408
shubhamsingh851 <shus@americana-food.com>
2025-06-24 16:35:55 +0530

    static

--- event_platform/settings.py
+++ event_platform/settings.py
@@ -12,10 +12,21 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from corsheaders.defaults import default_headers, default_methods
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # <--- This is the missing line!
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # Optional: for extra static dirs
+]
+
+# For production: serve static files efficiently
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -159,12 +170,5 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = "static/"
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
